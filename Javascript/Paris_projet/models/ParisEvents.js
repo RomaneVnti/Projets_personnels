7401f87c3380a7da63dddf7c4a22b7691fc7e10e
Romane Valenti <102488081+RomaneVnti@users.noreply.github.com>
2022-10-12 16:49:49 +0200

    Create ParisEvents.js

---
+++ Javascript/Paris_projet/models/ParisEvents.js
@@ -0,0 +1,36 @@
+
+export default class ParisEvents {
+        constructor(){
+            this.urlBase = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-'; 
+        }
+
+        getAll(keyword = '', dateStart = new Date().getFullYear(), sort = '', rows = 9, start) {
+
+            // c'est ici qu'on code le fetch pour récupérer les données
+            return fetch(`${this.urlBase}&rows=${rows}&q=${keyword}&refine.date_start=${dateStart}&sort=${sort}&start=${start}`)
+            .then(response => response.json())
+            .then( (data) => {
+    
+                return {
+                    nhits: data.nhits,                
+                    records: data.records.map((element) => { 
+                        return { 
+                        name:   element.fields.title,
+                        address : {
+                            name : element.fields.address_name,
+                            street : element.fields.address_street,
+                            zipcode : element.fields.address_zipcode,
+                            city : element.fields.address_city
+                        },
+                        image : {
+                            url : element.fields.cover_url,
+                            alt: element.fields.cover_alt
+                        },
+                        date : element.fields.date_description
+                        } 
+                    })
+                }
+            }); 
+        }
+        
+    }
