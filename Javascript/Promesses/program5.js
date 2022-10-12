372548cc7b7f5b8d74a093102fca379e698e4257
Romane Valenti <102488081+RomaneVnti@users.noreply.github.com>
2022-10-12 16:44:55 +0200

    Add files via upload

---
+++ Javascript/Promesses/program5.js
@@ -0,0 +1,9 @@
+const promise = new Promise(function (fulfill, reject) {
+    // Votre solution ici
+    fulfill("VALEUR DE LA PROMESSE");
+    
+  });
+
+  promise.then(console.log);
+
+  console.log("PROGRAMME PRINCIPAL");
